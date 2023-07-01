2947d702d0d8afdfa5665dd6839485276f337239
MilojevicMarko <marko.milojevic026@gmail.com>
2023-07-01 23:54:25 +0200

    second

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 let round = 0;
-let player1 = prompt("Unesite ime prvog igraca");
-let player2 = prompt("Unesite ime drugog igraca");
-let score1 = 0;
-let score2 = 0;
+let numberRound = prompt("Unesite broj zeljenih rundi");
+let player1 = {
+  name: prompt("Unesite prvog igraca"),
+  score1 : 0,
+  dice1: Math.floor(Math.random() * 6) + 1,
+};
+
+let player2 = {
+  name: prompt("Unesite drugog igraca"),
+  score2 : 0,
+  dice2: Math.floor(Math.random() * 6) + 1,
+};
+
 function game() {
-    round++;
-    let dice1 = Math.floor(Math.random() * 6) + 1;
-    let dice2 = Math.floor(Math.random() * 6) + 1;
-    score1 += dice1;
-    score2 += dice2;
+  round++;
+  // let dice1 = Math.floor(Math.random() * 6) + 1;
+  // let dice2 = Math.floor(Math.random() * 6) + 1;
+  player1.score1 += player1.dice1;
+  player2.score2 += player2.dice2;
+  console.log(
+    `%c*** Round ${round} *** \u{1F3B2}`,
+    "color:tomato; background:yellow;margin:5px;font-size:20px;"
+  );
+  console.log(`igrac ${player1.name} ${player1.dice1} - igrac ${player2.name} ${player2.dice2} - TOTAL SCORE ${player1.score1} - ${player2.score2}`) 
+
+  if (round === +numberRound) {
+    winner();
+  } else {
+    setTimeout(game, 1500)
+  }
+}
+
+function winner() {
+  if (player1.score1 > player2.score2) {
+    console.log(`%c*** WINNER *** ${player1.name}`, "font-size:30px;");
+  }
+  else if (player2.score2 > player1.score1) {
     console.log(
-      `%c*** Round ${round} *** \u{1F3B2}`,
-      "color:tomato; background:yellow;margin:5px;font-size:20px;"
+      `%c*** WINNER *** ${player2.name}`, "font-size:30px;text-align=center;"
     );
-    console.log(
-      `%c${player1} ${dice1} : ${dice2} ${player2}                                    (${score1}:${score2}`,"font-size:20px;"
-    );
-    if (round === 10) {
-      winner();
-    } else {
-			setTimeout(game, 1500)
-		}
+  }
+  else if (player2.score2 === player1.score1) {
+    console.log(`%c*** UNSOLVED - NO WINNER ****`, "font-size:30px;");
+  }
 }
 game();
-function winner() {
-  if (score1 > score2) {
-    console.log(`%c*** WINNER *** ${player1}`, "font-size:30px;");
-  }
-  else if (score2 > score1) {
-    console.log(
-      `%c*** WINNER *** ${player2}`,"font-size:30px;text-align=center;"
-    );
-  }
-	else if (score2 === score1) {
-		console.log(`%c*** UNSOLVED - NO WINNER ****`, "font-size:30px;");
-	}
-	
-}
+
